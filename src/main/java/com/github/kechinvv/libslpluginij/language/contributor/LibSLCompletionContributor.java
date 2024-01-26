d871d38591b181bc2fbc4a61d20f138c427348a0
kechin.vv <kechinv1@rambler.ru>
2024-01-26 21:36:43 +0300

    CompletionContributor capabilities test

--- src/main/java/com/github/kechinvv/libslpluginij/language/contributor/LibSLCompletionContributor.java
+++ src/main/java/com/github/kechinvv/libslpluginij/language/contributor/LibSLCompletionContributor.java
@@ -1,31 +1,32 @@
 package com.github.kechinvv.libslpluginij.language.contributor;
 
-import com.github.kechinvv.libslpluginij.antlr.LibSLLexer;
 import com.github.kechinvv.libslpluginij.antlr.LibSLParser;
-import com.github.kechinvv.libslpluginij.language.LibSL;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiErrorElement;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
-import static com.github.kechinvv.libslpluginij.language.LibSLParserDefinition.rules;
 import static com.github.kechinvv.libslpluginij.language.LibSLParserDefinition.tokens;
 
 final class LibSLCompletionContributor extends CompletionContributor {
     public static final Logger LOG = Logger.getInstance("ConfigureANTLRCompletion");
+
     LibSLCompletionContributor() {
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(tokens.get(LibSLLexer.Identifier)),
-                new CompletionProvider<>() {
-                    public void addCompletions(@NotNull CompletionParameters parameters,
-                                               @NotNull ProcessingContext context,
-                                               @NotNull CompletionResultSet resultSet) {
-                        LOG.info("try to complete " + parameters);
-                        resultSet.addElement(LookupElementBuilder.create("Hello"));
-                    }
-                }
-        );
+        var testProvider = new CompletionProvider<>() {
+            public void addCompletions(@NotNull CompletionParameters parameters,
+                                       @NotNull ProcessingContext context,
+                                       @NotNull CompletionResultSet resultSet) {
+                LOG.info("try to complete " + parameters);
+                resultSet.addElement(LookupElementBuilder.create("hello"));
+                resultSet.addElement(LookupElementBuilder.create("privet"));
+            }
+        };
+        var testPlace = PlatformPatterns.psiElement(tokens.get(LibSLParser.Identifier)).withParent(PsiErrorElement.class);
+
+        extend(CompletionType.BASIC, testPlace, testProvider);
     }
 
 
