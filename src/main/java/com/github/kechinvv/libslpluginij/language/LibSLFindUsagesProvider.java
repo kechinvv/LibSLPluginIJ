1dd90b615576a84e1ff7c3e183e0509210f5752d
kechinvv <valeraghst@gmail.com>
2024-01-29 13:48:09 +0300

    init find usages

---
+++ src/main/java/com/github/kechinvv/libslpluginij/language/LibSLFindUsagesProvider.java
@@ -0,0 +1,76 @@
+package com.github.kechinvv.libslpluginij.language;
+
+import com.github.kechinvv.libslpluginij.antlr.LibSLLexer;
+import com.github.kechinvv.libslpluginij.antlr.LibSLParser;
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner;
+import com.intellij.lang.cacheBuilder.WordsScanner;
+import com.intellij.lang.findUsages.FindUsagesProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import com.intellij.psi.tree.TokenSet;
+import org.antlr.intellij.adaptor.lexer.ANTLRLexerAdaptor;
+import org.antlr.intellij.adaptor.lexer.PSIElementTypeFactory;
+import org.antlr.intellij.adaptor.psi.ANTLRPsiNode;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+import static com.github.kechinvv.libslpluginij.language.LibSLParserDefinition.COMMENTS;
+import static com.github.kechinvv.libslpluginij.language.LibSLParserDefinition.tokens;
+
+public class LibSLFindUsagesProvider implements FindUsagesProvider {
+
+    @Nullable
+    @Override
+    public WordsScanner getWordsScanner() {
+ //       return null;
+        LibSLLexer lexer = new LibSLLexer(null);
+        var adapter = new ANTLRLexerAdaptor(LibSL.INSTANCE, lexer);
+        return new DefaultWordsScanner(adapter,
+                PSIElementTypeFactory.createTokenSet(LibSL.INSTANCE, LibSLLexer.Identifier),
+                COMMENTS,
+                TokenSet.EMPTY);
+    }
+
+    @Override
+    public boolean canFindUsagesFor(@NotNull PsiElement psiElement) {
+        //return psiElement instanceof ANTLRPsiNode;
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public String getHelpId(@NotNull PsiElement psiElement) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public String getType(@NotNull PsiElement element) {
+//        if (element instanceof SimpleProperty) {
+//            return "simple property";
+//        }
+        return "";
+    }
+
+    @NotNull
+    @Override
+    public String getDescriptiveName(@NotNull PsiElement element) {
+//        if (element instanceof SimpleProperty) {
+//            return ((SimpleProperty) element).getKey();
+//        }
+        return "";
+    }
+
+    @NotNull
+    @Override
+    public String getNodeText(@NotNull PsiElement element, boolean useFullName) {
+//        if (element instanceof SimpleProperty) {
+//            return ((SimpleProperty) element).getKey() +
+//                    SimpleAnnotator.SIMPLE_SEPARATOR_STR +
+//                    ((SimpleProperty) element).getValue();
+//        }
+        return "";
+    }
+
+}
