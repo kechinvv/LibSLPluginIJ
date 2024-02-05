43b4f919240d0829a215db3fe7d226947206b984
kechinvv <valeraghst@gmail.com>
2024-02-05 20:39:27 +0300

    inter-file usage provider in progress

--- src/main/java/com/github/kechinvv/libslpluginij/language/psi/rules/LslAutomatonDecl.java
+++ src/main/java/com/github/kechinvv/libslpluginij/language/psi/rules/LslAutomatonDecl.java
@@ -2,10 +2,14 @@ package com.github.kechinvv.libslpluginij.language.psi.rules;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class LslAutomatonDecl extends ASTWrapperPsiElement {
     public LslAutomatonDecl(@NotNull ASTNode node) {
@@ -16,12 +20,30 @@ public class LslAutomatonDecl extends ASTWrapperPsiElement {
     //public final Collection<LslFunctionDecl> functionDeclsList = PsiTreeUtil.findChildrenOfType(this, LslFunctionDecl.class);
     //public final Collection<LslProcDecl> procDeclsList = PsiTreeUtil.findChildrenOfType(this, LslProcDecl.class);
 
-    public @NotNull Collection<LslProcDecl> procDeclsList(){
+    public @NotNull Collection<LslProcDecl> procDeclsList() {
         return PsiTreeUtil.findChildrenOfType(this, LslProcDecl.class);
     }
 
-    public @NotNull Collection<LslFunctionDecl> functionDeclsList(){
+    public @NotNull Collection<LslFunctionDecl> functionDeclsList() {
         return PsiTreeUtil.findChildrenOfType(this, LslFunctionDecl.class);
+    }
+
+    public List<LslIdentifier> constructorFieldsName() {
+        Function<PsiElement, LslIdentifier> mapper = it ->
+                Objects.requireNonNull(PsiTreeUtil.findChildOfType(it, LslIdentifier.class));
+        return PsiTreeUtil
+                .findChildrenOfType(this, LslConstructorVariables.class).stream().map(mapper).toList();
+    }
+
+    public List<LslIdentifier> bodyFieldsName() {
+        Function<PsiElement, LslIdentifier> mapper = it ->
+                Objects.requireNonNull(PsiTreeUtil.findChildOfType(it, LslIdentifier.class));
+        return PsiTreeUtil
+                .findChildrenOfType(this, LslAutomatonStatement.class)
+                .stream()
+                .filter(it -> it.getFirstChild() instanceof LslVariableDecl)
+                .map(mapper)
+                .toList();
     }
 
 }
--- src/main/java/com/github/kechinvv/libslpluginij/language/psi/rules/LslIdentifierReference.java
+++ src/main/java/com/github/kechinvv/libslpluginij/language/psi/rules/LslIdentifierReference.java
@@ -1,13 +1,28 @@
 package com.github.kechinvv.libslpluginij.language.psi.rules;
 
+import com.github.kechinvv.libslpluginij.language.LibSLFileType;
 import com.github.kechinvv.libslpluginij.language.psi.LibSLPSIFileRoot;
+import com.github.kechinvv.libslpluginij.language.utils.LslUtils;
+import com.intellij.extapi.psi.ASTDelegatePsiElement;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.Consumer;
 import com.intellij.util.SmartList;
 import com.intellij.util.containers.SmartHashSet;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Function;
+
+import static com.intellij.psi.util.PsiTreeUtil.*;
+import static java.util.Objects.*;
 
 public class LslIdentifierReference extends PsiPolyVariantReferenceBase<LslIdentifier> {
     public static final Logger LOG = Logger.getInstance("Reference");
@@ -18,25 +33,13 @@ public class LslIdentifierReference extends PsiPolyVariantReferenceBase<LslIdent
 
     @Override
     public ResolveResult @NotNull [] multiResolve(boolean incompleteCode) {
-//        List<ResolveResult> results = new ArrayList<>();
-//        for (SimpleProperty property : properties) {
-//            results.add(new PsiElementResolveResult(property));
-//        }
-//        return results.toArray(new ResolveResult[0]);
         var declarations = new SmartList<PsiElement>();
-//        var declarations = new ArrayList<ResolveResult>();
         var name = myElement.getName();
         Consumer<PsiNamedElement> adder = it -> {
-//            LOG.info("it.getName = " + it.getName());
-//            LOG.info("myName = " + name);
-
-            if (it.getName().equals(name)) declarations.add(it);
+            if (Objects.equals(it.getName(), name)) declarations.add(it);
         };
         processDeclarations(adder);
-//        LOG.info("element = " + myElement.getName());
-//        LOG.info("declarations " + declarations);
-
-        return PsiElementResolveResult.createResults(declarations); //.toArray(new ResolveResult[0]);
+        return PsiElementResolveResult.createResults(declarations);
     }
 
 
@@ -48,8 +51,6 @@ public class LslIdentifierReference extends PsiPolyVariantReferenceBase<LslIdent
             if (name != null) variants.add(name);
         };
         processDeclarations(adder);
-//        LOG.info("element = " + myElement.getName());
-//        LOG.info("variants " + variants);
         return variants.toArray();
     }
 
@@ -60,42 +61,22 @@ public class LslIdentifierReference extends PsiPolyVariantReferenceBase<LslIdent
 
 
     private void processDeclarations(Consumer<PsiNamedElement> callback) {
-        PsiElement prev = myElement;
-        var current = myElement.getParent();
+        PsiElement varParent;
+        PsiElement prev = myElement.getPrevSibling();
 
-        while (current != null && !(prev instanceof LibSLPSIFileRoot)) {
-            //processDeclarationsIteration(current, callback);
-            prev = current;
-            current = current.getParent();
-            //if (current instanceof LslAutomatonDecl) break;
-            processDeclarationsIteration(current, callback);
+        if (prev != null && prev.getText().equals(".") && prev.getPrevSibling().getText().equals("this"))
+            processFieldDeclarationsInAutomaton(requireNonNull(getParentOfType(myElement, LslAutomatonDecl.class)), callback);
+        else if (getParentOfType(myElement, LslTypeIdentifier.class) != null)
+            findFiles(myElement.getProject(), callback);
+        else if (myElement.getParent() instanceof LslFunctionHeader || myElement.getParent() instanceof LslProcHeader) {
+        } else if ((varParent = getParentOfType(myElement, LslVariableDecl.class)) != null && varParent.getParent() instanceof LslAutomatonStatement) {
+        } else {
+            var automaton = getParentOfType(myElement, LslAutomatonDecl.class);
+            if (automaton != null) processDeclarationsInAutomaton(automaton, callback);
         }
+
     }
 
-    private void processDeclarationsIteration(PsiElement current, Consumer<PsiNamedElement> callback) {
-        switch (current) {
-//            case JavaScriptStatementsOwner -> processDeclarationsInScope(current, callback)
-//            case LslFunctionDecl functionDecl -> processDeclarationsInHeader(functionDecl, callback);
-//            case LslProcDecl procDecl -> processDeclarationsInHeader(procDecl, callback);
-            case LslAutomatonDecl automatonDecl -> processDeclarationsInAutomaton(automatonDecl, callback);
-//            case LslFunctionDecl functionDecl -> processDeclarationsInParameters(functionDecl, callback);
-//            is JavaScriptCatch -> {
-//                val assignable = current.assignable
-//                if (assignable != null) processDeclarationsInAssignable(assignable, callback)
-//            }
-//            is JavaScriptIterationStatement -> {
-//                val variableDeclarationList = when (current) {
-//                    is JavaScriptDoWhileStatement -> null
-//                    is JavaScriptWhileStatement -> null
-//                    is JavaScriptForStatement -> current.variableDeclarationList
-//                    is JavaScriptForInStatement -> current.variableDeclarationList
-//                    is JavaScriptForOfStatement -> current.variableDeclarationList
-//                }
-//                if (variableDeclarationList != null) processDeclarationsInVariableDeclarationList(variableDeclarationList, callback)
-            default -> {
-            }
-        }
-    }
 
     private void processDeclarationsInHeader(LslFunctionDecl functionDecl, Consumer<PsiNamedElement> callback) {
         var header = functionDecl.header();
@@ -110,7 +91,7 @@ public class LslIdentifierReference extends PsiPolyVariantReferenceBase<LslIdent
         if (header == null) return;
         var proc = header.methodName();
         if (proc == null) return;
-       callback.consume(proc);
+        callback.consume(proc);
     }
 
     private void processDeclarationsInAutomaton(LslAutomatonDecl automatonDecl, Consumer<PsiNamedElement> callback) {
@@ -118,6 +99,46 @@ public class LslIdentifierReference extends PsiPolyVariantReferenceBase<LslIdent
         automatonDecl.procDeclsList().forEach(procDecl -> processDeclarationsInHeader(procDecl, callback));
     }
 
+    private void processFieldDeclarationsInAutomaton(LslAutomatonDecl automatonDecl, Consumer<PsiNamedElement> callback) {
+        automatonDecl.constructorFieldsName().forEach(callback::consume);
+        automatonDecl.bodyFieldsName().forEach(callback::consume);
+    }
+
+    private void findFiles(Project project, Consumer<PsiNamedElement> callback) {
+        Collection<VirtualFile> virtualFiles =
+                FileTypeIndex.getFiles(LibSLFileType.INSTANCE, GlobalSearchScope.allScope(project));
+        for (VirtualFile virtualFile : virtualFiles) {
+
+            LibSLPSIFileRoot file = (LibSLPSIFileRoot) PsiManager.getInstance(project).findFile(virtualFile);
+            if (file != null) {
+                LOG.info("FILE = " + LslUtils.getAllSiblings(file));
+                var typeDefs = getChildrenOfType(file, LslTypeDefBlock.class);
+                if (typeDefs != null) {
+                    LOG.info("type = " + Arrays.stream(typeDefs).map(LslTypeDefBlock::getText).toList());
+                    typeNameResolver(typeDefs, LslTypeDefBlock.class, callback);
+                }
+                else {
+                    var automatons = getChildrenOfType(file, LslAutomatonDecl.class);
+                    if (automatons != null) typeNameResolver(automatons, LslAutomatonDecl.class, callback);
+                }
+            }
+        }
+    }
+
+    private <T extends PsiElement> void typeNameResolver(T[] elements, Class<T> klass, Consumer<PsiNamedElement> callback) {
+        Function<T, LslIdentifier> mapper = it -> {
+            var typeName = getChildOfType(it, klass);
+            if (typeName != null) return getChildOfType(typeName, LslIdentifier.class);
+            else return null;
+        };
+        Arrays.stream(elements).map(mapper).filter(Objects::nonNull).forEach(it -> {callback.consume(it);
+            LOG.info("type = " + it.getName());
+        });
+    }
 }
+
+
+
+
 
 
