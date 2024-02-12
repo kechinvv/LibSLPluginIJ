375eacc3cde16c5a24d0a54c212bae0b751eef48
kechinvv <valeraghst@gmail.com>
2024-02-12 19:18:19 +0300

    fixed highlight import added highlight include

--- src/main/java/com/github/kechinvv/libslpluginij/language/highlight/LibSLAnnotator.java
+++ src/main/java/com/github/kechinvv/libslpluginij/language/highlight/LibSLAnnotator.java
@@ -7,29 +7,44 @@ import com.github.kechinvv.libslpluginij.language.utils.LslUtils;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.util.PsiTreeUtil;
 import org.antlr.intellij.adaptor.lexer.TokenIElementType;
+import org.jetbrains.annotations.NotNull;
 
 
 final class LibSLAnnotator implements Annotator {
+    public static final Logger LOG = Logger.getInstance("Annotator");
+
+    private final TextRange importRange = new TextRange(0, 6);
+    private final TextRange includeRange = new TextRange(0, 7);
 
     @Override
-    public void annotate(PsiElement element, AnnotationHolder holder) {
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
         if (element instanceof LslAnnotationUsage)
             LslUtils.getFilteredSiblings(element.getFirstChild(), it -> !(it instanceof LslAnnotationArgs))
-                    .forEach(it -> highlight(it, holder));
-
+                    .forEach(it -> highlightRule(it, holder));
+        else if (element.getNode().getElementType() instanceof TokenIElementType tokenIElementType)
+            switch (tokenIElementType.getANTLRTokenType()) {
+                case LibSLLexer.ImportStatement -> highlightTextRange(element, holder, importRange);
+                case LibSLLexer.IncludeStatement -> highlightTextRange(element, holder, includeRange);
+            }
     }
 
 
-    public void highlight(PsiElement element, AnnotationHolder holder) {
+    public void highlightRule(PsiElement element, AnnotationHolder holder) {
         holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-                .range(element).textAttributes(DefaultLanguageHighlighterColors.METADATA).create();
+                .range(element)
+                .textAttributes(DefaultLanguageHighlighterColors.METADATA)
+                .create();
     }
 
-
+    public void highlightTextRange(PsiElement element, AnnotationHolder holder, TextRange subRange) {
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .range(element.getTextRange().cutOut(subRange))
+                .textAttributes(DefaultLanguageHighlighterColors.KEYWORD)
+                .create();
+    }
 }
