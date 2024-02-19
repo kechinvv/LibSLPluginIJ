4c32347bdfd2a7c5b224a6b7a630710830ad19ea
kechinvv <valeraghst@gmail.com>
2024-02-19 18:53:22 +0300

    init imports check for find usages

--- src/main/java/com/github/kechinvv/libslpluginij/language/psi/LibSLPSIFileRoot.java
+++ src/main/java/com/github/kechinvv/libslpluginij/language/psi/LibSLPSIFileRoot.java
@@ -1,5 +1,6 @@
 package com.github.kechinvv.libslpluginij.language.psi;
 
+import com.github.kechinvv.libslpluginij.antlr.LibSLLexer;
 import com.github.kechinvv.libslpluginij.language.LibSL;
 import com.github.kechinvv.libslpluginij.language.LibSLFileType;
 import com.github.kechinvv.libslpluginij.language.psi.rules.*;
@@ -8,11 +9,17 @@ import com.intellij.extapi.psi.PsiFileBase;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.psi.FileViewProvider;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.antlr.intellij.adaptor.lexer.TokenIElementType;
 import org.jetbrains.annotations.*;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
+import static com.github.kechinvv.libslpluginij.language.LibSLParserDefinition.tokens;
 import static com.intellij.psi.util.PsiTreeUtil.*;
 
 public class LibSLPSIFileRoot extends PsiFileBase implements PsiElement {
@@ -61,5 +68,27 @@ public class LibSLPSIFileRoot extends PsiFileBase implements PsiElement {
                 }).filter(Objects::nonNull).toList();
     }
 
+    public Collection<PsiElement> getImports() {
+        return findChildrenOfType(this, LslGlobalStatement.class).stream()
+                .map(PsiElement::getFirstChild)
+                .filter(it -> it.getNode().getElementType() instanceof TokenIElementType tok
+                        && tok.getANTLRTokenType() == LibSLLexer.ImportStatement)
+                .toList();
+    }
+
+    public Map<String, String> getImportsPathsMap() {
+        var importsMap = new HashMap<String, String>();
+        var imports = getImports();
+        imports.forEach(it -> {
+            var paths = it.getText().split("[ ;]");
+            if (paths.length < 2) return;
+            var path = paths[1];
+            var folders = path.split("/");
+            if (folders.length == 0) return;
+            var fileName = folders[folders.length - 1];
+            importsMap.put(fileName, path);
+        });
+        return importsMap;
+    }
 
 }
