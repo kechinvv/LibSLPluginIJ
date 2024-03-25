14537c5733dcee3a83f649f7d4e587cae52c4467
kechin.vv <kechinv1@rambler.ru>
2024-03-25 13:59:00 +0300

    LibSL module detection for actions

--- src/main/java/com/github/kechinvv/libslpluginij/actions/TranslateAction.java
+++ src/main/java/com/github/kechinvv/libslpluginij/actions/TranslateAction.java
@@ -5,15 +5,23 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class TranslateAction extends AnAction {
 
     @Override
     public void update(AnActionEvent e) {
         var psi = e.getData(CommonDataKeys.PSI_FILE);
+        var virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
+        var project = e.getData(CommonDataKeys.PROJECT);
+        var isLslModule = false;
+        if (project != null && virtualFile != null)
+            isLslModule = Objects.equals(ProjectFileIndex.getInstance(project).getModuleForFile(virtualFile).getModuleTypeName(), "LIBSL_MODULE");
         boolean isLsl = (psi != null) && (psi.getFileType() == LibSLFileType.INSTANCE);
-        e.getPresentation().setEnabledAndVisible(isLsl);
+        e.getPresentation().setEnabledAndVisible(isLsl || isLslModule);
     }
 
     @Override
